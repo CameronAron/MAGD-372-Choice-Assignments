5b4ccc9e281ca3ceea3dd1e7505092e00b6743ce
CameronAron <cameron.aron04@gmail.com>
2025-10-08 14:15:53 -0500

    updated player spawning

--- Assets/Scripts/playerSpawn.cs
+++ Assets/Scripts/playerSpawn.cs
@@ -5,13 +5,28 @@ public class playerSpawn : MonoBehaviour
 {
     public GameObject playerPrefab;
     public Transform p1Spawn, p2Spawn;
+    private bool p1In = false;
+    private bool p2In = false;
 
     void Start()
     {
-        var player1 = PlayerInput.Instantiate(prefab: playerPrefab, playerIndex: 0, controlScheme: "KeyboardOne", pairWithDevice: Keyboard.current, splitScreenIndex: 0);
-        var player2 = PlayerInput.Instantiate(prefab: playerPrefab, playerIndex: 1, controlScheme: "KeyboardTwo", pairWithDevice: Keyboard.current, splitScreenIndex: 1);
+        
+    }
 
-        player1.transform.position = p1Spawn.position;
-        player2.transform.position = p2Spawn.position;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1) && !p1In)
+        {
+            var player1 = PlayerInput.Instantiate(prefab: playerPrefab, playerIndex: 0, controlScheme: "KeyboardOne", pairWithDevice: Keyboard.current, splitScreenIndex: 0);
+            player1.transform.position = p1Spawn.position;
+            p1In = true;
+        }
+
+        if(Input.GetKey(KeyCode.Alpha2) && !p2In)
+        {
+            var player2 = PlayerInput.Instantiate(prefab: playerPrefab, playerIndex: 1, controlScheme: "KeyboardTwo", pairWithDevice: Keyboard.current, splitScreenIndex: 1);
+            player2.transform.position = p2Spawn.position;
+            p2In = true;
+        }
     }
 }
